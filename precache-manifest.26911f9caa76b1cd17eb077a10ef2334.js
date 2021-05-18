3282da5b1bdb580cf2d40190cbce7d3b33023f09
Juanita Vélez <juanitavelez@Juanitas-MacBook-Air.local>
2021-05-18 14:44:40 -0500

    Updates

---
+++ precache-manifest.26911f9caa76b1cd17eb077a10ef2334.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5c63c0df3d885b25593",
+    "url": "/robofriends-react/static/css/main.c1142d4d.chunk.css"
+  },
+  {
+    "revision": "c5c63c0df3d885b25593",
+    "url": "/robofriends-react/static/js/main.fdba6f5e.chunk.js"
+  },
+  {
+    "revision": "7598b0760c4f1a805ebd",
+    "url": "/robofriends-react/static/js/runtime~main.26d36195.js"
+  },
+  {
+    "revision": "623492b001b4fe5e3bfd",
+    "url": "/robofriends-react/static/css/2.92cbfa80.chunk.css"
+  },
+  {
+    "revision": "623492b001b4fe5e3bfd",
+    "url": "/robofriends-react/static/js/2.f2c8162a.chunk.js"
+  },
+  {
+    "revision": "f2c121df5bc05f03f3ea",
+    "url": "/robofriends-react/static/js/3.b1248334.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-react/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "bddc398982c487d1d199b56eaf61cbca",
+    "url": "/robofriends-react/index.html"
+  }
+];
